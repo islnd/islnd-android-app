c6b16ff3ca49b051359685d175c58147ae5e4c66
David Thompson <daviddt17@gmail.com>
2016-02-01 18:42:13 -0500

    Close add comment edittext on successful submit

--- app/src/main/java/com/island/island/Activities/ViewPostActivity.java
+++ app/src/main/java/com/island/island/Activities/ViewPostActivity.java
@@ -1,11 +1,13 @@
 package com.island.island.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -57,6 +59,8 @@ public class ViewPostActivity extends AppCompatActivity
         EditText addCommentEditText = (EditText) findViewById(R.id.post_comment_edit_text);
         String commentText = addCommentEditText.getText().toString();
 
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
         if(commentText.equals(""))
         {
             Snackbar.make(view, getString(R.string.empty_comment_post),
@@ -65,6 +69,8 @@ public class ViewPostActivity extends AppCompatActivity
         else
         {
             IslandDB.addCommentToPost(post, new Comment("", commentText));
+            addCommentEditText.setText("");
+            imm.hideSoftInputFromWindow(addCommentEditText.getWindowToken(), 0);
         }
     }
 }
