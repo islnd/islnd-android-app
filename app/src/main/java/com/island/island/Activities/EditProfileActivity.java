0d59b35c0ade35f9f8ab7330e024f51c5dea55ca
David Thompson <daviddt17@gmail.com>
2016-02-05 13:54:03 -0500

    Added image selector for edit profile

--- app/src/main/java/com/island/island/Activities/EditProfileActivity.java
+++ app/src/main/java/com/island/island/Activities/EditProfileActivity.java
@@ -1,5 +1,7 @@
 package com.island.island.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +19,9 @@ public class EditProfileActivity extends AppCompatActivity
 {
     private Profile profile;
     private EditText aboutMe;
+
+    private static final int SELECT_PROFILE_IMAGE = 1;
+    private static final int SELECT_HEADER_IMAGE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,6 +42,24 @@ public class EditProfileActivity extends AppCompatActivity
         aboutMe.setText(profile.getAboutMe());
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (resultCode == RESULT_OK)
+        {
+            if (requestCode == SELECT_PROFILE_IMAGE)
+            {
+                // TODO: set profile image
+                Uri selectedImageUri = data.getData();
+            }
+            else if(requestCode == SELECT_HEADER_IMAGE)
+            {
+                // TODO: set header image
+                Uri selectedImageUri = data.getData();
+            }
+        }
+    }
+
     public void saveProfile(View view)
     {
         // TODO: handle image stuff for saving
@@ -44,5 +67,23 @@ public class EditProfileActivity extends AppCompatActivity
         IslandDB.updateProfile(profile);
 
         Snackbar.make(view, getString(R.string.profile_saved), Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void chooseProfileImage(View view)
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_image)),
+                SELECT_PROFILE_IMAGE);
+    }
+
+    public void chooseHeaderImage(View view)
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_image)),
+                SELECT_HEADER_IMAGE);
     }
 }
