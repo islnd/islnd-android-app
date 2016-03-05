5174f1c4d035ca789c3ff433c818211c5e11dd40
David Thompson <daviddt17@gmail.com>
2016-03-05 17:20:14 -0500

    Cleaned up manifest and renamed content authority

--- app/src/main/java/io/islnd/android/islnd/app/database/IslndContract.java
+++ app/src/main/java/io/islnd/android/islnd/app/database/IslndContract.java
@@ -7,7 +7,7 @@ import android.provider.BaseColumns;
 
 public class IslndContract {
 
-    public static final String CONTENT_AUTHORITY = "com.island.island.Database";
+    public static final String CONTENT_AUTHORITY = "io.islnd.android.islnd.app.database";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_POST = "post";
     public static final String PATH_USER = "user";
